a4051bbd21c1f8420ec88f9fe907505782b8b338
aberezowski <ajrberezowski@gmail.com>
2022-03-25 13:10:31 -0600

    Started working on Hamper

--- src/edu/ucalgary/ensf409/Hamper.java
+++ src/edu/ucalgary/ensf409/Hamper.java
@@ -1,4 +1,31 @@
 package edu.ucalgary.ensf409;
 
-public class Hamper {
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Hamper extends SelectFood{
+    private HashMap<String, Integer> calorieNeeds = new HashMap<>();
+    private HashMap<String, Integer> wasteAmount;
+    private ArrayList<Integer> hamperFoods;
+    private HashMap<Integer, Object> avaliableFoods = new HashMap<>();
+
+    public Hamper(HashMap<String, Integer> calorieNeeds, HashMap<Integer, Object> avaliableFoods){
+        this.calorieNeeds.putAll(calorieNeeds);
+        this.avaliableFoods.putAll(avaliableFoods);
+        this.hamperFoods = calculateFoods(this.calorieNeeds, this.avaliableFoods);
+        this.wasteAmount = calculateWaste(this.hamperFoods, this.calorieNeeds);
+    }
+
+    public HashMap<String, Integer> getWaste(){
+        return this.wasteAmount;
+    }
+
+    public ArrayList<Integer> getFoodQuantities(){
+        return this.hamperFoods;
+    }
+
+    public HashMap<String, Integer> getCalorieNeeds() {
+        return this.calorieNeeds;
+    }
 }
--- src/edu/ucalgary/ensf409/SelectFood.java
+++ src/edu/ucalgary/ensf409/SelectFood.java
@@ -1,4 +1,14 @@
 package edu.ucalgary.ensf409;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class SelectFood {
+    public static ArrayList<Integer> calculateFoods(HashMap<String, Integer> needs, HashMap<Integer, Object> avaliableFoods){
+
+    }
+
+    public static HashMap<String, Integer> calculateWaste(ArrayList<Integer> foods, HashMap<String, Integer> needs){
+
+    }
 }
