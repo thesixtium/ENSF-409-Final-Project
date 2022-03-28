9c8dd88036e2f1c166f8fe01ad09970b5dda1524
aberezowski <ajrberezowski@gmail.com>
2022-03-28 14:08:41 -0600

    Update HouseholdTest.java

--- src/edu/ucalgary/ensf409/HouseholdTest.java
+++ src/edu/ucalgary/ensf409/HouseholdTest.java
@@ -1,5 +1,18 @@
 package edu.ucalgary.ensf409;
 
+import org.junit.Test;
+
 public class HouseholdTest {
+
+    /*
+    @Test
+    public void testOtherNeeds(){
+        assertEquals("Did not return correct other needs", 40, testClient.otherNeeds());
+    }
+     */
+
+    //Household can init
+
+    //getFamily returns stuff
 
 }
