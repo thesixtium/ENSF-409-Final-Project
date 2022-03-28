0ca5c7be7657079b87bdd821386d1de534b4e108
aberezowski <ajrberezowski@gmail.com>
2022-03-28 14:29:31 -0600

    Update HouseholdTest.java

--- src/edu/ucalgary/ensf409/HouseholdTest.java
+++ src/edu/ucalgary/ensf409/HouseholdTest.java
@@ -1,18 +1,58 @@
+/**
+ * @author Aleksander Berezowski
+ * @version 134
+ * @since 1.0
+ */
+
 package edu.ucalgary.ensf409;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.ArrayList;
 
 public class HouseholdTest {
 
-    /*
+    private ArrayList<Object> testObjs = new ArrayList<>();
+    private Household testObj = HouseholdTest(testObjs);
+
     @Test
-    public void testOtherNeeds(){
-        assertEquals("Did not return correct other needs", 40, testClient.otherNeeds());
+    public void testHouseholdInit(){
+        assertNotEquals("Initialzed object is null", testObj, null);
     }
-     */
 
-    //Household can init
+    @Test
+    public void testGetFamily(){
+        ArrayList<Object> expectedFamily = new ArrayList<>();
+        ArrayList<Object> testFamily = testObj.getFamily();
 
-    //getFamily returns stuff
+        assertEquals("getFamily does not return correct array list", expectedFamily, testFamily);
+    }
+
+    @Test
+    public void testGetFamilyHamper(){
+        Hamper expectedHamper = new Hamper();
+        Hamper testHamper = testObj.getFamilyHamper();
+
+        assertEquals("getFamilyHamper does not return correct array list", expectedHamper, testHamper);
+    }
+
+    @Test
+    public void testSetFamily(){
+        ArrayList<Object> expectedFamily = new ArrayList<>();
+        ArrayList<Object> testFamily = testObj.setFamily(expectedFamily).getFamily();
+
+        assertEquals("setFamily does not return correct array list", expectedFamily, testFamily);
+    }
+
+    @Test
+    public void testSetFamilyHamper(){
+        Hamper expectedHamper = new Hamper();
+        Hamper testHamper = testObj.setFamilyHamper(expectedHamper).getFamilyHamper();
+
+        assertEquals("getFamilyHamper does not return correct array list", expectedHamper, testHamper);
+    }
+
 
 }
