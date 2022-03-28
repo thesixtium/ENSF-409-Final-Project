da5e3b7c780b33d11a56f27165cbed8fe4ec19eb
danijourdain <danijourdain@icloud.com>
2022-03-28 12:27:18 -0600

    Adding Test files
    Worked on tests for Foods, Hamper, and OrderForm

---
+++ src/edu/ucalgary/ensf409/HamperTest.java
@@ -0,0 +1,58 @@
+/**
+ * @author Danielle Jourdain
+ * @version 1.0
+ * @since 1.0
+ */
+
+package edu.ucalgary.ensf409;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class HamperTest {
+    private int[] calorieValues = {100, 200, 300, 400};
+    
+    /**
+     * Hamper(int[]) is called to create a Hamper object.
+     */
+    @Test
+    public void testConstructor() {
+        Hamper test = new Hamper(calorieValues);
+
+        assertNotNull(test, "Hamper constructor did not correctly create a Hamper obejct.");
+    }
+
+    /**
+     * Test that the getCalorieNeeds method returns an integer array
+     */
+    @Test
+    public void testGetFoodQuantities() {
+        Hamper test = new Hamper(calorieValues);
+        int[] result = test.getCalorieNeeds();
+
+        assertNotNull(result, "getCalorieNeeds did not return a value.");
+    }
+
+    /**
+     * Test that getWaste method returns an integer array
+     */
+    @Test
+    public void testGetWaste() {
+        Hamper test = new Hamper(calorieValues);
+        int[] result = test.getWaste();
+
+        assertNotNull(result, "getCalorieNeeds did not return a value.");
+    }
+
+    /**
+     * Test that getFoodQuantities returns an ArrayList of integers
+     */
+    @Test
+    public void testGetCalorieNeeds() {
+        Hamper test = new Hamper(calorieValues);
+        ArrayList<Integer> result = test.getFoodQuantities();
+
+        assertNotNull(result, "getCalorieNeeds did not return a value.");
+    }
+}
