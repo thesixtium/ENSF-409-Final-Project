cc724ebc64332ee1cd74a340d4b5758d2d8045e7
danijourdain <danijourdain@icloud.com>
2022-04-04 14:54:21 -0600

    Updated documentation/comments for OrderForm

--- src/edu/ucalgary/ensf409/OrderForm.java
+++ src/edu/ucalgary/ensf409/OrderForm.java
@@ -2,7 +2,7 @@
  * @author Aleksander Berezowski
  * @author Danielle Jourdain
  * @author Philippa Madill
- * @version 1.2
+ * @version 1.5
  * @since 1.0
  */
 
@@ -24,18 +24,29 @@ public class OrderForm {
      */
     public OrderForm(RequestForm formInfo) {
         this.ORDER = formInfo;
+        //set the RequestForm object
+
         this.filename = "orderForm";
         File testFile = new File(this.filename + ".txt");
         int count = 1;
+
         while(testFile.isFile()) {
-            this.filename = this.filename + 1;
+            //if the File orderForm.txt already exists, see if orderForm1.txt exists
+            //if that exists, try orderForm2.txt, and so on until a valid filename is found
+            this.filename = this.filename + Integer.toString(count);
             testFile = new File(this.filename + ".txt");
             count++;
         }
+
         this.filename = this.filename + ".txt";
+        //set the final filename with the .txt extension
     }
 
-    public void createForm(String filename) {
+    /**
+     * This method creates a text file containing the information for all of the
+     * households and hampers in the current order.
+     */
+    public void createForm() {
         StringBuilder formResult = new StringBuilder();
         formResult.append("The Peanut Butter Scenario Food Bank\nHamper Order Form\n\n");
         formResult.append("Name:\nDate:\n");
@@ -46,6 +57,9 @@ public class OrderForm {
         Iterator<Household> houseIter = households.iterator();
         int counter = 0;
         while(houseIter.hasNext()) {
+            //iterate through the households, printing the info about each
+            //on a new line
+
             Household curHouse = houseIter.next();
             formResult.append("Household " + counter + ": ");
             formResult.append(countHousehold(curHouse) + "\n");
@@ -56,27 +70,39 @@ public class OrderForm {
         Iterator<Hamper> hamperIter = hampers.iterator();
         counter = 1;
         while(hamperIter.hasNext()) {
+            //iterate through the hampers 
             result.append("Hamper " + counter + " items:\n");
             Hamper tempHamper = hamperIter.next();
             
             if(tempHamper.getEnoughFood()) {
                 result.append(successfulHamperCreation(tempHamper));
+                //if the hamper creation was successful, add that info to the result
             }
             else {
                 result.append(unsuccessfulHamperCreation(tempHamper));
+                //otherwise add what the hamper was short by to the result
             }
             formResult.append("\n\n");
         }
 
+
+        formResult.delete(formResult.length() - 2, formResult.length());
+        //remove the extra newline character
         writeToFile(formResult.toString());
     }
 
+    /**
+     * This method counts the number of each type of family member.
+     * @param house The household to count members for.
+     * @return A string containing all of the information for the household
+     */
     private String countHousehold(Household house) {
         int[] values = new int[4];
         ArrayList<Object> family = house.getFamily();
         Iterator<Object> famIter = family.iterator();
 
         while(famIter.hasNext()) {
+            //iterate through the family/
             Object person = famIter.next();
             String type = person.getClass().getName();  //gives "edu.ucalgary.ensf409.classname"
             //get the string representation of the class of the person object
@@ -159,6 +185,12 @@ public class OrderForm {
     }
     */
 
+    /**
+     * This method creates a string contiaining the info for a hamper that was
+     * successfully created.
+     * @param hamper The hamper to get information for.
+     * @return A string containing the hamper information.
+     */
     private String successfulHamperCreation(Hamper hamper) {
         StringBuilder result = new StringBuilder();
         HashMap<Integer, FoodData> foods = hamper.getFoodQuantities();
@@ -172,12 +204,22 @@ public class OrderForm {
         return result.toString();
     }
 
+    /**
+     * This method creates a string containing the info for a hamper that was
+     * unsuccessfullly created.
+     * @param hamper The hamper to get informtaion for.
+     * @return A string containing what the hamper is short by.
+     */
     private String unsuccessfulHamperCreation(Hamper hamper) {
         StringBuilder result = new StringBuilder();
 
         return result.toString();
     }
 
+    /**
+     * This method writes a message to a text file.
+     * @param message The message to write to the file.
+     */
     private void writeToFile(String message) {
         try {
             FileWriter writer = new FileWriter(filename);
