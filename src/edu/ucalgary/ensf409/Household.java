6c66bb3e962132436f28d9411016bd57ca76a5c2
aberezowski <ajrberezowski@gmail.com>
2022-04-09 12:31:11 -0600

    Documentation on Household.java

--- src/edu/ucalgary/ensf409/Household.java
+++ src/edu/ucalgary/ensf409/Household.java
@@ -11,17 +11,43 @@ package edu.ucalgary.ensf409;
 import java.util.HashMap;
 
 public class Household {
-    String[] familyList;
-    Hamper familyHamper;
+    String[] familyList; // Stores the list of family members in a given household
+    Hamper familyHamper; // The hamper that goes to that specific household
 
+    /**
+     * Constructor for Household class
+     * @param clientData Is a hashmap with a String key identifying the client name,
+     *                   and the value being a secondary hashmap. For the secondary
+     *                   hashmap the String key is the name of the food category
+     *                   (ie. other) and the value being the integer value.
+     * @param familyList Is an array of Strings specifying the family. The possible
+     *                   Strings that can be included in this array is "Adult Male",
+     *                   "Adult Female", "Child Under 8", and "Child Over 8".
+     * @param availableFoods Is a hashmap with a key of the identifying integer of
+     *                       a food (this should be the primary key from the initial
+     *                       SQL table) and the value is a FoodData object, which
+     *                       stores all needed values that we could want for a
+     *                       specific food.
+     */
     public Household(HashMap<String, HashMap<String, Integer>> clientData,
                      String[] familyList,
-                     HashMap<Integer, FoodData> avaliableFoods){
+                     HashMap<Integer, FoodData> availableFoods){
         initializeClients(clientData);
+
+        // Set the internal variables
         this.familyList = familyList;
-        this.familyHamper = new Hamper(calculateNeeds(), avaliableFoods);
+        this.familyHamper = new Hamper(calculateNeeds(), availableFoods);
     }
 
+    /**
+     * Method to initialize all the static clients so that we can use them to
+     * pull values for each category (ei. other).
+     *
+     * @param clientData Is a hashmap with a String key identifying the client name,
+     *                   and the value being a secondary hashmap. For the secondary
+     *                   hashmap the String key is the name of the food category
+     *                   (ie. other) and the value being the integer value.
+     */
     private void initializeClients(HashMap<String, HashMap<String, Integer>> clientData){
         HashMap<String, Integer> client = clientData.get("Adult Male");
         AdultMale.setTotalCalories(client.get("Total Calories"));
@@ -52,6 +78,11 @@ public class Household {
         ChildUnder8.setOtherPercent(client.get("Other Percent"));
     }
 
+    /**
+     * Method to calculate the needs of a household given a String array.
+     *
+     * @return A HouseholdNeeds object that is then passed to a Hamper initializer.
+     */
     private HouseholdNeeds calculateNeeds(){
         HouseholdNeeds returnNeeds = new HouseholdNeeds();
         for(String i : familyList){
@@ -80,10 +111,18 @@ public class Household {
         return returnNeeds;
     }
 
+    /**
+     * Getter to return a household's Hamper
+     * @return Hamper object
+     */
     public Hamper getFamilyHamper() {
         return familyHamper;
     }
 
+    /**
+     * Getter to return a household's family list as a String array
+     * @return String[] familyList
+     */
     public String[] getFamilyList() {
         return familyList;
     }
