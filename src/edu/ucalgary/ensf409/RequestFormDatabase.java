96e301459bc9719dfc3581230175fee6c5cb09c2
Philippamadill <philippamadill@gmail.com>
2022-04-09 22:12:28 -0600

    fixed some things and attempted to make documentation

--- src/edu/ucalgary/ensf409/RequestFormDatabase.java
+++ src/edu/ucalgary/ensf409/RequestFormDatabase.java
@@ -46,17 +46,32 @@ public class RequestFormDatabase{
 		return this.PASSWORD;
 	}
 	
+	/**
+	This method returns the sorted caloric requirements of different 
+	types of clients so they can be used by other classes.
+	
+	@return clientValues	The caloric requirements of each type of 
+							client sorted and available for use by 
+							other classes and methods.
+	*/
+	
 	public static HashMap<String, HashMap<String, Integer>> getClientValues(){
 		return clientValues;
 	}
 	
-	public void setClientValues(String tableName){
+	/**
+	This method accesses the DAILY_CLIENT_NEEDS table in the database
+	to retreive the daily needed caloric values of each type of client
+	and sort them accordingly so that these values can be used elsewhere
+	*/
+	
+	public void setClientValues(){
 		clientValues = new HashMap<String, HashMap<String, Integer>>();
 
 		HashMap<String, Integer> temp = new HashMap<String, Integer>();
 		try{
 			Statement myStmt = dbConnect.createStatement();
-			results = myStmt.executeQuery("SELECT * FROM " + tableName);
+			results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS");
 			while(results.next()){
 				temp.put("grain", results.getInt("WholeGrains"));
 				temp.put("fv", results.getInt("FruitVeggies"));
@@ -71,15 +86,29 @@ public class RequestFormDatabase{
 		}
 	}
 	
+	/**
+	This method retreives the HashMap of the caloric values 
+	of available foods so this information can be used elsewhere.
+	
+	@return foodValues	A HashMap of integer and FoodData objects that 
+						can be accessed elsewhere in the package.
+	*/
+	
 	public static HashMap<Integer, FoodData> getFoodValues(){
 		return foodValues;
 	}
-	public void setFoodValues(String tableName){
+	
+	/**
+	This method takes the caloric values of the available foods in 
+	the database and puts them in a HashMap so they're more easily 
+	accessible by other classes and methods
+	*/
+	public void setFoodValues(){
 		foodValues = new HashMap<Integer, FoodData>();
 		FoodData temp = new FoodData<>();
 		try{
 			Statement myStmt = dbConnect.createStatement();
-			results = myStmt.executeQuery("SELECT * FROM " + tableName);
+			results = myStmt.executeQuery("SELECT * FROM AVAILABLE_FOOD");
 			while(results.next()){
 				temp.put("grain", results.getInt("GrainContent"));
 				temp.put("fv", results.getInt("FVContent"));
@@ -93,6 +122,14 @@ public class RequestFormDatabase{
 			ex.printStackTrace();
 		}
 	}
+	
+	/**
+	This method, when provided an integer key, will remove the 
+	food entry in the database associated with that key.
+	
+	@param key Is an integer corresponding to the ItemID
+				of any particular food in the database.
+	*/
 	
 	public void removeFood(int key){
 		
@@ -108,6 +145,10 @@ public class RequestFormDatabase{
         }
 		
 	}
+	
+	/**
+	This method closes the connection to the database.
+	*/
 	
 	  public void close() {
 
