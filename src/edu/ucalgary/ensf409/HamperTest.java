3fd57e24e736b3cab075935b5a07a82503264adb
danijourdain <danijourdain@icloud.com>
2022-04-12 20:26:10 -0600

    Mostly finished HamperTest file

--- src/edu/ucalgary/ensf409/HamperTest.java
+++ src/edu/ucalgary/ensf409/HamperTest.java
@@ -1,6 +1,6 @@
 /**
  * @author Danielle Jourdain
- * @version 1.6
+ * @version 1.10
  * @since 1.0
  */
 
@@ -10,137 +10,108 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
 
+/* NOTE: PLEASE RESET THE DATABASE BEFORE RUNNING THESE TESTS */
+
 public class HamperTest {
-    HouseholdNeeds needs;
-    HashMap<Integer, FoodData> availableFoods;
+    /**
+     * This tests if the constructor creates a Hamper object as needed
+     */
+    @Test 
+    public void testConstructor() {
+        HouseholdNeeds hn = initHouseholdNeeds();
+        HashMap<Integer, FoodData> availableFood = updateFoodValues();
+        Hamper hamper = new Hamper(hn, availableFood);
+
+        assertNotNull("Hamper constructor did not create an object", hamper);
+    }
 
     /**
-     * Tests that the constructor creates an object
+     * This tests if the getWaste method returns a HashMap of the correct size
      */
     @Test
-    public void testConstructor() {
-        Hamper hamper = new Hamper(needs, availableFoods);
-
-        assertNotNull("Hamper constructor did not create an object.", hamper);
-    }
-
-    /**
-     * Tests that the Hamper containts the correct food item when called with
-     * enough food for the HouseholdNeeds
-     */
-    @Test 
-    public void testGetFoodQuantities() {
-        Hamper hamper = new Hamper(needs, availableFoods);
-        HashMap<Integer, FoodData> result = hamper.getFoodQuantities();
-
-        HashMap<Integer, FoodData> expected = new HashMap<>();
-        FoodData pizza = new FoodData("Frozen pizza, pepperoni", 680, 49, 219, 267);
-        expected.put(68, pizza);
-
-        assertEquals("Hamper did not contain the correct number of items.", 1, result.size());
-        assertEquals("Hamper did not contain expected food values", expected, result);
-    }
-
-    /**
-     * Tests that the getWaste method returns the correct result
-     * when there is enough food.
-     */
-    @Test   
     public void testGetWaste() {
-        Hamper hamper = new Hamper(needs, availableFoods);
-        HashMap<String, Integer> result = hamper.getWaste();
+        HouseholdNeeds hn = initHouseholdNeeds();
+        HashMap<Integer, FoodData> availableFood = updateFoodValues();
+        Hamper hamper = new Hamper(hn, availableFood);
+        HashMap<String, Integer> waste = hamper.getWaste();
 
-        boolean correct = true;
-        for(Integer i: result.values()) {
-            if(i != 0) {
-                correct = false;
-            }
-        }
-
-        assertTrue("getWaste did not return the correct waste values.", correct);
+        assertNotNull("getWaste did not return an object", waste);
+        assertEquals("getWaste did not create a HashMap of the right size", 4, waste.size());
     }
 
     /**
-     * tests that the getCalorieNeeds method returns the same HouseholdNeeds object
-     * that was stored when the hamper was created.
+     * This tests if the getFoodQuantities method returns an object.
+     * The specific values cannot be tested since they will change depeding on what
+     * order the tests are run, and how recently the database was reset.
      */
-    @Test   
+    @Test
+    public void testGetFoodQuantities() {
+        HouseholdNeeds hn = initHouseholdNeeds();
+        HashMap<Integer, FoodData> availableFood = updateFoodValues();
+        Hamper hamper = new Hamper(hn, availableFood);
+        HashMap<Integer, FoodData> foods = hamper.getFoodQuantities();
+
+        assertNotNull("getFoodQuantities did not return an object", foods);
+    }
+
+    /**
+     * This tests if the getCalorieNeeds method returns the expected HouseholdNeeds object.
+     */
+    @Test
     public void testGetCalorieNeeds() {
-        Hamper hamper = new Hamper(needs, availableFoods);
+        HouseholdNeeds hn = initHouseholdNeeds();
+        HashMap<Integer, FoodData> availableFood = updateFoodValues();
+        Hamper hamper = new Hamper(hn, availableFood);
         HouseholdNeeds actual = hamper.getCalorieNeeds();
 
-        assertEquals("getCalorieNeeds did not return the correct result.", this.needs, actual);
+        assertEquals("getCalorieNeeds did not return the expected object", hn, actual);
     }
 
     /**
-     * This test checks the value of the getShortBy method when there
-     * is enough food.
+     * This tests if the boolean from getEnoughFood is true when there is enough food
+     * to create a hamper
      */
     @Test   
-    public void testGetShortByEnoughFood() {
-        Hamper hamper = new Hamper(needs, availableFoods);
-        FoodData actual = hamper.getShortBy();
+    public void testGetEnoughFood() {
+        HouseholdNeeds hn = initHouseholdNeeds();
+        HashMap<Integer, FoodData> availableFood = updateFoodValues();
+        Hamper hamper = new Hamper(hn, availableFood);
+        boolean result = hamper.getEnoughFood();
 
-        FoodData expected = new FoodData("Short By");
-        assertEquals("getShortBy did not return correct result", expected, actual);
+        assertTrue("getEnoughFood did not return true", result);
     }
 
     /**
-     * This tests the value of the getShortBy method when there is not 
-     * enough food.
+     * This tests that the shortBy field is null when there is enough food
+     * to create a hamper.
      */
-    @Test  
-    public void testGetShortByNotEnoughFood() {
-        availableFoods.clear();
-        FoodData shortItem = new FoodData("Short pizza", 600, 49, 219, 267);
-        availableFoods.put(1, shortItem);
+    @Test
+    public void testGetShortBy() {
+        HouseholdNeeds hn = initHouseholdNeeds();
+        HashMap<Integer, FoodData> availableFood = updateFoodValues();
+        Hamper hamper = new Hamper(hn, availableFood);
+        FoodData shortBy = hamper.getShortBy();
 
-        Hamper hamper = new Hamper(needs, availableFoods);
-
-        FoodData actual = hamper.getShortBy();
-
-        FoodData expected = new FoodData("Short By", 680, 49, 219, 267);
-        assertEquals("getShortBy did not return correct result", expected, actual);
+        assertNull("getShortBy returned an object even though there was enough food", shortBy);
     }
 
     /**
-     * Tests that the available foods from the hamper matches the
-     * values that were given upon constructing the object
+     * This tests that the getAvailableFoods method returns a HashMap. Again, the specifics cannot
+     * be tested since values are updated after every Hamper creation.
      */
-    @Test 
+    @Test
     public void testGetAvailableFoods() {
-        Hamper hamper = new Hamper(needs, availableFoods);
+        HouseholdNeeds hn = initHouseholdNeeds();
+        HashMap<Integer, FoodData> availableFood = updateFoodValues();
+        Hamper hamper = new Hamper(hn, availableFood);
         HashMap<Integer, FoodData> actual = hamper.getAvailableFoods();
+        availableFood = updateFoodValues();
 
-        assertEquals("getAvailableFoods did not return correct result", this.availableFoods, actual);
+        assertNotNull("getAvailableFoods did not return an object", actual);
     }
 
     /**
-     * Tests that the boolean shortBy returns the correct value
-     * when there is enough food.
-     */
-    @Test 
-    public void testGetEnoughFoodGood() {
-        Hamper hamper = new Hamper(needs, availableFoods);
-        boolean result = hamper.getEnoughFood();
-        assertTrue("getEnoughFood did not return correct result", result);
-    }
-
-    /**
-     * tests that the boolean shortBy has the right value when there is
-     * not enough food.
-     */
-    @Test 
-    public void testGetEnoughFoodBad() {
-        availableFoods.clear();
-
-        Hamper hamper = new Hamper(needs, availableFoods);
-        boolean result = hamper.getEnoughFood();
-        assertFalse("getEnoughFood did not return correct result", result);
-    }
-
-    /**
-     * Helper method to setup connection to the database before running tests.
+     * Helper method to set up a database connection.
      */
     @BeforeClass
     public static void setupDatabase() {
@@ -151,63 +122,28 @@ public class HamperTest {
 		database.close();
     }
 
-    @Before
-    public void setup() {
-        this.availableFoods = RequestFormDatabase.getFoodValues();
-        this.needs = new HouseholdNeeds();
-        this.needs.changeFvCalories(680);
-        this.needs.changeGrainCalories(49);
-        this.needs.changeProteinCalories(219);
-        this.needs.changeOtherCalories(267);
-        //setup the HouseholdNeeds obejct 
-        //these calorie needs line up exactly to item 68 in the database
-            //to simplify testing
-    }
+    /**
+     * Helper method to update the stored food values after being used
+     * @return
+     */
+    private HashMap<Integer, FoodData> updateFoodValues() {
+        RequestFormDatabase requestForm = new RequestFormDatabase("jdbc:mysql://localhost/FOOD_INVENTORY","student","ensf" );
+		requestForm.initializeConnection();
+		requestForm.setFoodValues();
+		requestForm.setClientValues();
 
-    /*
-    private int[] calorieValues = {100, 200, 300, 400};
+        return RequestFormDatabase.getFoodValues();
+    }
     
-    /*
-     * Hamper(int[]) is called to create a Hamper object.
-     
-    @Test
-    public void testConstructor() {
-        Hamper test = new Hamper(calorieValues);
-
-        assertNotNull(test, "Hamper constructor did not correctly create a Hamper obejct.");
-    }
-
     /**
-     * Test that the getCalorieNeeds method returns an integer array
-     
-    @Test
-    public void testGetFoodQuantities() {
-        Hamper test = new Hamper(calorieValues);
-        int[] result = test.getCalorieNeeds();
-
-        assertNotNull(result, "getCalorieNeeds did not return a value.");
+     * Helper method to set the HouseholdNeeds
+     */
+    public static HouseholdNeeds initHouseholdNeeds() {
+        HouseholdNeeds hn = new HouseholdNeeds();
+        hn.changeFvCalories(100);
+        hn.changeGrainCalories(100);
+        hn.changeProteinCalories(100);
+        hn.changeOtherCalories(100);
+        return hn;
     }
-
-    /**
-     * Test that getWaste method returns an integer array
-     
-    @Test
-    public void testGetWaste() {
-        Hamper test = new Hamper(calorieValues);
-        int[] result = test.getWaste();
-
-        assertNotNull(result, "getCalorieNeeds did not return a value.");
-    }
-
-    /**
-     * Test that getFoodQuantities returns an ArrayList of integers
-     
-    @Test
-    public void testGetCalorieNeeds() {
-        Hamper test = new Hamper(calorieValues);
-        ArrayList<Integer> result = test.getFoodQuantities();
-
-        assertNotNull(result, "getCalorieNeeds did not return a value.");
-    }
-    */
 }
